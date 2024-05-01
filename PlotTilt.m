4ca3f141cf7c1c7583e60cdc727cdc407ca75f64
Kristin Dickerson <kdickerson97@gmail.com>
2024-05-01 07:13:27 -0700

    Add new

---
+++ PlotTilt.m
@@ -0,0 +1,38 @@
+%%% =======================================================================
+%%   Purpose: 
+%       This function plots the tilt data from the tap file. These plots 
+%       are found under the Penetration Data tab on SlugHeat GUI. If no 
+%       tap file exists, the average depth will be used and the user will 
+%       be notified with a pop-up window.
+%%   Last edit:
+%       01/13/2024 by Kristin Dickerson, UCSC 
+%%% =======================================================================
+
+function PlotTilt(TAPRecord, ...
+            figure_Main, ...
+            TiltMean, ...
+            Tilt, ...
+            checkbox_TiltPlot, ...
+            grid_PenetrationInfo, ...
+            axes_Tilt, ...
+            Reprocess)
+
+ TAPRecord = 1:1:length(TAPRecord);  
+
+% Plot tilt sensor data
+% ------------------------
+    if isempty(TAPRecord) || length(Tilt) == 1
+        if Reprocess == 1
+            uialert(figure_Main,['There is no tilt and pressure (TAP) record.' ...
+                fprintf('\n') 'Average tilt = ' num2str(TiltMean) '°'], ['File' ...
+                ' not found'], 'Icon', 'warning');
+        end
+            checkbox_TiltPlot.Value = 0;
+            checkbox_TiltPlot.Enable = 'off';
+            grid_PenetrationInfo.RowHeight = {'1x', '1x', '0x', '0x'};   
+    else
+        plot(axes_Tilt, TAPRecord, Tilt, '-r', 'MarkerSize',3);
+        axes_Tilt.Color = [0.94,0.94,0.94];
+    end
+    
+    drawnow;
---
+++ cbValueChange.m
@@ -0,0 +1,63 @@
+%%% ======================================================================
+%%  Purpose: 
+%       This is the callback function for sensor checkboxes. When a 
+%       sensor's checkbox is 'on', the temperature measurements recorded 
+%       by that sensor are plotted on the Temperature relative to bottom
+%       water vs. record number plot. When a sensor's checkbox is 'off', 
+%       the data recorded by that sensor is hidden on the plot. This only 
+%       controls the DISPLAY of the data. Must push the Remove Sensors 
+%       button totur actually process without the data recorded by this sensor.
+%%  Last edit:
+%       01/22/2024 by Kristin Dickerson, UCSC
+%%% ======================================================================
+
+function cbValueChange(~,src,plot, plot2, plot3, plot4, plot5, plot6, ...
+    plot7, plot8, plot9, plot10, plot11, plot12, plot13, plot14,  ...
+    checkbox1, checkbox2, checkbox3)
+    
+%% If checkbox is unchecked, turn off all plots with this data
+    if src.Value == 0              
+            plot.Visible = 'off';
+            if ishghandle(plot2, 'line')
+                plot2.Visible = 'off';
+                plot3.Visible = 'off';
+                plot4.Visible = 'off';
+                plot5.Visible = 'off';
+                plot6.Visible = 'off';
+                plot7.Visible = 'off';
+                plot8.Visible = 'off';
+                plot9.Visible = 'off';
+                plot10.Visible = 'off';
+                plot11.Visible = 'off';
+                plot12.Visible = 'off';
+                plot13.Visible = 'off';
+                plot14.Visible = 'off';
+                checkbox1{1}.Value = 0; 
+                checkbox2{1}.Value = 0;
+                checkbox3{1}.Value = 0; 
+            end
+    
+%% Else if f checkbox is checked, turn on all plots with this data
+    else                          
+            plot.Visible = 'on';
+            if ishghandle(plot2, 'line')
+                plot2.Visible = 'on';
+                plot3.Visible = 'on';
+                plot4.Visible = 'on';
+                plot5.Visible = 'on';
+                plot6.Visible = 'on';
+                plot7.Visible = 'on';
+                plot8.Visible = 'on';
+                plot9.Visible = 'on';
+                plot10.Visible = 'on';
+                plot11.Visible = 'on';
+                plot12.Visible = 'on';
+                plot13.Visible = 'on';
+                plot14.Visible = 'on';
+                checkbox1{1}.Value = 1; 
+                checkbox2{1}.Value = 1;
+                checkbox3{1}.Value = 1; 
+            end
+    
+    end
+    drawnow;
