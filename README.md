105c6e74bd6cae378fcae314c628963c6458d610
Kristin Dickerson <kdickerson97@gmail.com>
2024-06-03 07:35:32 -0700

    delete current

--- README.md
+++
@@ -1,2 +0,0 @@
-# slugheat
-working repo for processing software for data collected by a heat-flow measurement system
--- inputs/Old_HFData/OldFormats/MiddleValley/MV95H61/makedir.bat
+++
@@ -1,6 +0,0 @@
-md aug97files
-move *.res aug97files
-move *.hf aug97files
-move *.re2 aug97files
-move *.bps aug97files
-
